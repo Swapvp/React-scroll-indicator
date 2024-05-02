b59d9e01907eadb3574f9d7bd25f9237fa44cdc0
Swapvp <swapneshpawar1994@gmail.com>
2024-05-02 10:23:36 +0530

    scroll-indicator

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ScrollIndicator from "./Components/ScrollIndicator";
+
+function App() {
+  return (
+    <>
+      <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} />
+    </>
+  );
+}
+
+export default App;
